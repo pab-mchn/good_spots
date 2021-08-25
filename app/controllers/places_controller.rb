6abbeaf1c53b1cb847e3733f1734eb4bd5d030be
pablo michelini <pab.michelini@gmail.com>
2021-08-25 14:37:58 +0200

    actual conflict resolution

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,11 +1,5 @@
 class PlacesController < ApplicationController
   def overview
-<<<<<<< HEAD
-  end
-
-  def show
-=======
     @tags = Tag.all.select { |tag| tag.name.length < 8 }.sample(9)
->>>>>>> d69f7478abbd234fdbd2a711a3098a65cba1f948
   end
 end
