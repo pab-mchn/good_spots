97df66449dd702b7c0ae6b06d8eb953022a1877b
pablo michelini <pab.michelini@gmail.com>
2021-08-25 16:25:32 +0200

    new css clases in show page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,4 +2,13 @@ class PlacesController < ApplicationController
   def overview
     @tags = Tag.all.select { |tag| tag.name.length < 8 }.sample(9)
   end
+
+  def show
+    @place = Place.find(params[:id])
+    if params[:query].present?
+      # Todo -> Get all places with that query
+    else
+      @bottom_places = Place.all.sample(6)
+    end
+  end
 end
