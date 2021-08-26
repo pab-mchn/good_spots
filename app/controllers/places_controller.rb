c0869eeb39972d256988fb42750ab0b6534a9a41
pablo michelini <pab.michelini@gmail.com>
2021-08-26 16:47:42 +0200

    overview cards dinamic

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,13 @@
 class PlacesController < ApplicationController
   def overview
     @tags = Tag.all.select { |tag| tag.name.length < 8 }.sample(9)
+
+    if params[:query].present?
+      # Todo -> Get all places with that query
+    else
+      @new_places = Place.all.sample(6)
+      @recomended_places = Place.all.sample(6)
+    end
   end
 
   def show
@@ -10,7 +17,8 @@ class PlacesController < ApplicationController
     else
       @bottom_places = Place.all.sample(6)
     end
-  
+  end
+
   def index
 
   end
