ce0910d6bc0298c813ec76cf44a7481d5b6c01ea
XueYing Chou <x.chou@gmx.de>
2021-08-30 12:25:35 +0200

    viewing-controller

---
+++ app/controllers/viewings_controller.rb
@@ -0,0 +1,12 @@
+class ViewingsController < ApplicationController
+
+  def viewing
+    if 
+    
+    else
+      
+    end
+    viewings = viewing.all
+  end
+  #index for all the viewings which are liked
+end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,6 +24,7 @@ require("channels")
 
 // External imports
 import "bootstrap";
+import "@fortawesome/fontawesome-free/css/all";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
